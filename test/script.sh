f2ba478c09df1567d4dbd6d24843b020c5ec13a5
Tarek Djebali <tarekdj@gmail.com>
2020-12-27 18:49:02 +0100

    Update script.sh

--- test/script.sh
+++ test/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./vendor/bin/phpcs --standard=test/phpcs.xml test/file.php | grep -q "FOUND 3 ERRORS AFFECTING 3 LINES"
+./vendor/bin/phpcs --standard=test/phpcs.xml test/file.php | grep -q "FOUND 3 ERRORS AFFECTING 30 LINES"
 ./vendor/bin/phpcbf --standard=test/phpcs.xml test/file.php | grep -q "A TOTAL OF 3 ERRORS WERE FIXED IN 1 FILE"
