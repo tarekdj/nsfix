80bbb22a3cc4cdb81855a38deb53a9d461b052fb
Tarek Djebali <tarekdj@gmail.com>
2020-12-28 16:47:11 +0100

    Update file.php

--- test/file.php
+++ test/file.php
@@ -3,3 +3,13 @@ namespace Tarekdj;
 
 use Tarekdj\Dummy;
 use Tarekdj\Fake;
+
+$test = new \Tarekdj\DummyClass()
+
+class MyClass extends \Tarekdj\MyDummyClass implements \Tarekdj\DummyInterface
+{
+    public function __construct(\Tarekdj\Fake $fake)
+    {
+
+    }
+}
